e29fa1561f0a761b643b19f95588b9d15dbadff3
Khuong Tran <dbcgdhh@gmail.com>
2021-04-29 12:57:11 +0700

    Show bet and name to poker table

--- src/js/room.js
+++ src/js/room.js
@@ -9,13 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   socket.emit("join", username);
   socket.on("refreshRoom", users => {
-    console.log("Users:", users);
+    _setUpSeats(users);
   });
 });
 
 function _getUsername() {
   const pageUrl = new URL(window.location.href);
   return pageUrl.searchParams.get("user") || "";
+}
+
+function _setUpSeats(users) {
+  const table = document.getElementById("table");
+  table.innerHTML = "";
+
+  users.forEach(user => {
+    const bet = user.bet || "?";
+    const name = user.name;
+    const li = document.createElement("li");
+    li.innerHTML = `<div>${bet}</div><p>${name}</p>`;
+    table.appendChild(li);
+  });
 }
 
 function bet(value) {
