0db972b236c8ebdafeb6fca144e0c8b69db2cfb9
Khuong Tran <dbcgdhh@gmail.com>
2021-04-29 10:58:39 +0700

    Init io send message

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static("src"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/src/public/index.html");
 });
 
 io.on("connection", socket => {
-  console.log("a user connected");
+  socket.on("chat message", msg => {
+    console.log("message: " + msg);
+  });
 });
 
 server.listen(3000, () => {
--- src/js/front.js
+++ src/js/front.js
@@ -1,1 +1,12 @@
 const socket = io();
+
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
