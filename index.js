7ff92a01d2caaf51b4390e42026bf5cc08b1c0cd
Khuong Tran <dbcgdhh@gmail.com>
2021-04-29 18:28:37 +0700

    Fix heroku port

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
