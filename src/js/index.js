8c18ddab6541f232e2291e0350a97528be2b8788
Khuong Tran <dbcgdhh@gmail.com>
2021-05-10 15:03:18 +0700

    Implement poker_join event

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,11 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const username = document.getElementById("name").value;
+  _sendJoinEventToUsergram(username);
   window.location.href = `${window.location.origin}/room?user=${username}`;
 });
+
+function _sendJoinEventToUsergram(username) {
+  ugattr["serviceId"] = username;
+  usergram.push(["send", "Ugo97x-1", "event", "poker_join", ugattr]);
+}
